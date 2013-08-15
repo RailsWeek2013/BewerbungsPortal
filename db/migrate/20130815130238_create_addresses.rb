7dd77c6175cae447e7fa7336aca3b6ab72460151
Ilja Michajlow <ilja.michajlow@mni.thm.de>
2013-08-15 15:33:59 +0200

    User and Address classes are ready

---
+++ db/migrate/20130815130238_create_addresses.rb
@@ -0,0 +1,12 @@
+class CreateAddresses < ActiveRecord::Migration
+  def change
+    create_table :addresses do |t|
+      t.string :street
+      t.string :zip
+      t.string :city
+      t.references :user, index: true
+
+      t.timestamps
+    end
+  end
+end
