5cab8e78dd35355fb6d2e6a4a5ae32bcc27cb67f
Ilja Michajlow <ilja.michajlow@mni.thm.de>
2013-08-15 11:57:20 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BewerbungsPortal::Application.config.secret_key_base = 'a52d30c48d8016300e1a4aeedcaed765fde9895bda61efa21e84b3b2dfab91ec713e596bf8e9bdc5c36642d460941264107f0f338e7c5d843410f3bc6c96346f'
